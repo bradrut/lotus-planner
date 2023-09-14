9d4c4f1af6dab1ccaa6f8f52e78b3f624e651d32
Brad Rutkowski <rutkowski.bradley@gmail.com>
2023-09-14 13:44:18 -0400

    Completed first iteration content and styling on the About page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
   import "../app.css";
 
+  // TODO: Fix bug where active link can become out of sync on UI after hard refresh
   let navLinks = [
     { href: "/", linkText: "Lotus Planner", active: true },
     { href: "/about", linkText: "About", active: false }
@@ -30,10 +31,10 @@
   }
 </script>
 
-<div class="drawer">
+<div class="drawer h-full">
   <input id="nav-drawer" type="checkbox" bind:checked={drawerOpen} class="drawer-toggle" />
-  <div class="drawer-content">
-    <div class="flex-none hamburger-container">
+  <div class="drawer-content h-full">
+    <div class="flex-none hamburger-container absolute">
       <label for="nav-drawer" class="btn btn-square btn-ghost drawer-button">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20" class="inline-block w-5 h-5 stroke-current">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
-<h1 class="text-lg">Lotus Planner</h1>
-<div>
-  <p>This is where the core Lotus Planner will live</p>
+<div class="grid h-full grid-cols-3 gap-4 content-center">
+  <div class="col-span-3">
+    <h1 class="text-2xl text-center">Lotus Planner</h1>
+    <p class="text-center">This is where the lotus planner will live</p>
+  </div>
 </div>
 
 <style lang="postcss">
